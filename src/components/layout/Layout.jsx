137593b76b5e15cdeac52f77ef5752e76fd53f42
emilegascoin <emilegascoin@gmail.com>
2026-05-04 10:04:41 +1000

    feat: vercel analytics

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import Nav from './Nav'
 import Footer from './Footer'
 import ChatWidget from './ChatWidget'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout() {
   return (
